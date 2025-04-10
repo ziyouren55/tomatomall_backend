50cb75d482aff5f1194a31df88603550a1568283
cczznn <1405754855@qq.com>
2025-04-10 17:59:42 +0800

    lab4-czn

--- src/main/java/com/example/tomatomall/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tomatomall.repository;
-
-import com.example.tomatomall.po.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Integer> {
-}
--- src/main/java/com/example/tomatomall/service/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.example.tomatomall.service;
-
-import com.example.tomatomall.vo.shopping.PaymentResponseVO;
-
-public interface OrderService {
-    PaymentResponseVO payInit(Integer orderId);
-
-    void updateOrderStatus(String orderId, String alipayTradeNo, String amount);
-}
